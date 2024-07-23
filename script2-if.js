de57c759697453fb93c2a529abdd0e5c1c8bcd92
edineladelso <edineladelso@users.noreply.github.com>
2024-07-23 17:15:34 +0000

    Adição dos arquivos do projeto

---
+++ script2-if.js
@@ -0,0 +1,9 @@
+const dwe = window.document.getElementById('dwe')
+dwe.style.position = 'absolute';
+dwe.style.zIndex = '-3';
+function aparecer(){
+dwe.style.zIndex = '3';}
+
+function desaparecerFormulario(){
+    formulario.style.zIndex = "-3"
+}
