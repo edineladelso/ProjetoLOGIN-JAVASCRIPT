a4fe92a43f51909b46e023007f7598456ef879d3
edineladelso <edineladelso@users.noreply.github.com>
2024-07-24 14:33:04 +0000

    Atualizacao final

--- script2-if.js
+++
@@ -1,9 +0,0 @@
-const dwe = window.document.getElementById('dwe')
-dwe.style.position = 'absolute';
-dwe.style.zIndex = '-3';
-function aparecer(){
-dwe.style.zIndex = '3';}
-
-function desaparecerFormulario(){
-    formulario.style.zIndex = "-3"
-}
